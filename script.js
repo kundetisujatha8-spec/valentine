565c47fff20002e00b2a09fe471d8f200074b11d
kundetisujatha8-spec <kundetisujatha8@gmail.com>
2026-02-13 15:13:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function){
+document.addEventListener("DOMContentLoaded", function(){
+    
     const video = document.getElementById("videoPlayer");
-  const message = document.getElementById("message");
-  
-  const sadSteps = [
+    const message = document.getElementById("message");
+    const sadSteps = [
   { text: "Are you sure? 😢", video: "sad.mp4" },
   { text: "Think once 💔", video: "sad1.mp4" },
   { text: "Don't break my heart 💘", video: "sad2.mp4" },
@@ -14,15 +14,17 @@ let noClickCount = 0;
 
 window.yesClick = function() {
   video.src = happyVideo;
+  video.load();
   video.play();
   message.innerHTML = "Yayyy 💖 i knew it 😍 Luv Uh Banda Bangaram🤗!";
-}
+};
 
 window.noClick = function() {
   // Order lo video + message
   const step = sadSteps[noClickCount];
 
   video.src = step.video;
+  video.load();
   video.play();
   message.innerHTML = step.text;
 
@@ -32,4 +34,5 @@ window.noClick = function() {
   if (noClickCount >= sadSteps.length) {
     noClickCount = 0;
   }
+};
 });
